c36c29338eeaad1d73b0f853a809b932aa51bc03
Aurelien Rebourg <aurelien.rebourg@gmail.com>
2020-04-03 02:26:14 +0200

    adding declaration types

---
+++ react-lightweight-charts.d.ts
@@ -0,0 +1,20 @@
+declare module "react-lightweight-charts-wrapper" {
+    interface Props {
+        candlestickSeries?: Array<any>,
+        lineSeries?: Array<any>,
+        areaSeries?: Array<any>,
+        barSeries?: Array<any>,
+        histogramSeries?: Array<any>,
+        width?: number,
+        height?: number,
+        options?: object,
+        autoWidth?: boolean,
+        from?: number,
+        to?: number,
+        onClick?: void,
+        onCrosshairMove?: void,
+        onTimeRangeMove?: void
+    }
+
+    export default class ChartWrapper extends React.Component<Props> {}
+}
