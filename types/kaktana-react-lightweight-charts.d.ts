d3050a8deec8c53a2943299d92bf00a0b5a49b88
mfbz <michael.fabozzi@icloud.com>
2020-10-10 19:49:54 +0200

    fix: fixed indentation back to 4 spaces

--- types/kaktana-react-lightweight-charts.d.ts
+++ types/kaktana-react-lightweight-charts.d.ts
@@ -1,29 +1,29 @@
 import {
-  MouseEventHandler,
-  TimeRangeChangeEventHandler,
+    MouseEventHandler,
+    TimeRangeChangeEventHandler,
 } from "lightweight-charts";
 import React from "react";
 
 declare module "kaktana-react-lightweight-charts" {
-  interface Props {
-    candlestickSeries?: Array<any>;
-    lineSeries?: Array<any>;
-    areaSeries?: Array<any>;
-    barSeries?: Array<any>;
-    histogramSeries?: Array<any>;
-    width?: number;
-    height?: number;
-    options?: object;
-    autoWidth?: boolean;
-    autoHeight?: boolean;
-    legend?: string;
-    from?: number;
-    to?: number;
-    onClick?: MouseEventHandler;
-    onCrosshairMove?: MouseEventHandler;
-    onTimeRangeMove?: TimeRangeChangeEventHandler;
-    darkTheme?: boolean;
-  }
+    interface Props {
+        candlestickSeries?: Array<any>;
+        lineSeries?: Array<any>;
+        areaSeries?: Array<any>;
+        barSeries?: Array<any>;
+        histogramSeries?: Array<any>;
+        width?: number;
+        height?: number;
+        options?: object;
+        autoWidth?: boolean;
+        autoHeight?: boolean;
+        legend?: string;
+        from?: number;
+        to?: number;
+        onClick?: MouseEventHandler;
+        onCrosshairMove?: MouseEventHandler;
+        onTimeRangeMove?: TimeRangeChangeEventHandler;
+        darkTheme?: boolean;
+    }
 
-  export default class ChartWrapper extends React.Component<Props> {}
+    export default class ChartWrapper extends React.Component<Props> {}
 }
